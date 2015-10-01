950ae976fd5e057183f86549fd6384f1b8640f2c
Vlad <vladrotaru@elasticorange.com>
2015-10-01 14:52:55 +0300

    Canon Landing page - ver 1

---
+++ README.md
@@ -0,0 +1,1 @@
+# canon-soho-landingpage
---
+++ js/main.js
@@ -0,0 +1,30 @@
+// $(function(){
+// $('#myCarousel').carousel({
+//   interval: 40000
+// });
+
+// $('.carousel .item').each(function(){
+//   var next = $(this).next();
+//   if (!next.length) {
+//     next = $(this).siblings(':first');
+//   }
+//   next.children(':first-child').clone().appendTo($(this));
+
+//   if (next.next().length>0) {
+ 
+//       next.next().children(':first-child').clone().appendTo($(this)).addClass('rightest');
+      
+//   }
+//   else {
+//       $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+     
+//   }
+// });
+// })
+
+$(document).ready(function(){
+    $('.autoplay').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1
+  });
+});
